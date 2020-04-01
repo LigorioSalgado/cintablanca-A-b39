546f3c8e753968173f696db6cc60ce7e0444b810
Ligorio Edwin Salgado Flores <ligorioedwinsalgadoflores@MacBook-Pro-de-Ligorio.local>
2020-03-31 18:53:50 -0600

    fixes

--- javascript/script.js
+++ javascript/script.js
@@ -1,165 +1,167 @@
-console.log("Hola Mundo!");//Esto imprime en consola hola mundo
-/*  Este es un comentario multilinea  */
+// console.log("Hola Mundo!");//Esto imprime en consola hola mundo
+// /*  Este es un comentario multilinea  */
 
-var nombre = "Edwin"; //String
+// var nombre = "Edwin"; //String
 
-console.log(nombre)
-console.log("Nombre es del tipo ", typeof(nombre));
+// console.log(nombre)
+// console.log("Nombre es del tipo ", typeof(nombre));
 
-var edad = 26;  //Integer o entero
+// var edad = 26;  //Integer o entero
 
-console.log(edad);
-console.log("Edad es del tipo ", typeof(edad));
+// console.log(edad);
+// console.log("Edad es del tipo ", typeof(edad));
 
-var tiene_lentes = true; //Boolean solo puede ser true o false
+// var tiene_lentes = true; //Boolean solo puede ser true o false
 
-console.log(tiene_lentes); 
-console.log("tiene_lentes es del tipo ", typeof(tiene_lentes));
+// console.log(tiene_lentes); 
+// console.log("tiene_lentes es del tipo ", typeof(tiene_lentes));
 
-var peso = 78.6; //Flotante o Float
+// var peso = 78.6; //Flotante o Float
 
-console.log(peso);
-console.log("peso es del tipo ", typeof(peso));
+// console.log(peso);
+// console.log("peso es del tipo ", typeof(peso));
 
-//tinpos de dato especiales
+// //tinpos de dato especiales
 
-null // Vacio
-undefined // No esta definico
-Infinity // Divisdite entre 0 y es un numero infinito
-NaN // Not at Number
+// null // Vacio
+// undefined // No esta definico
+// Infinity // Divisdite entre 0 y es un numero infinito
+// NaN // Not at Number
 
-var x = null;
+// var x = null;
 
-x = 8;
+// x = 8;
 
-//Cases 
+// //Cases 
 
-//Camel Case
+// //Camel Case
 
-var tieneCoronavirus = false;
+// var tieneCoronavirus = false;
 
-//snake case
+// //snake case
 
-var tiene_lentes = true;
+// var tiene_lentes = true;
 
-// Operaciones
-var suma =  3 + 4;
+// // Operaciones
+// var suma =  3 + 4;
 
-var resta = 9 - 5;
+// var resta = 9 - 5;
 
-var multiplicacion = 6 * 5;
+// var multiplicacion = 6 * 5;
 
-var division = 18 / 9;
+// var division = 18 / 9;
 
-console.log("La suma es",suma);
-console.log("La resta es", resta);
-console.log("La multiplicacion es",multiplicacion);
-console.log("La division es",division);
+// console.log("La suma es",suma);
+// console.log("La resta es", resta);
+// console.log("La multiplicacion es",multiplicacion);
+// console.log("La division es",division);
 
-var x = 4;
-var y = 5;
+// var x = 4;
+// var y = 5;
 
-//Acumuladores
-//x =  x + y 
-console.log(x += y);
-//x = x - y
-console.log(x -= y);
-//x = x * y
-console.log(x *= y);
+// //Acumuladores
+// //x =  x + y 
+// console.log(x += y);
+// //x = x - y
+// console.log(x -= y);
+// //x = x * y
+// console.log(x *= y);
 
-//Residuo
-console.log(y % x);
+// //Residuo
+// console.log(y % x);
 
-x = 6; // Numeber
+// x = 6; // Numeber
 
-y = "7" // String
+// y = "7" // String
 
-console.log(x + y); // Concateno  67
+// console.log(x + y); // Concateno  67
 
-y = "hola"
+// y = "hola"
 
-console.log(y + x); // hola6
+// console.log(y + x); // hola6
 
-y = "7"
+// y = "7"
 
-console.log(x - y); // -1
+// console.log(x - y); // -1
 
-y = "hola"
+// y = "hola"
 
-console.log(x - y); //NaN
-
-
-
-// Operadores Logicos
-
-var edad = 18;
-
-// Comparación por valor
-
-console.log("La comparación por valor de edad es :", edad == 18); //True
-
-// Comparacion por tupo y valor
-console.log("La comparación por tipo de edad es :", edad === "18"); //False
-
-// Diferente por valor
-
-console.log("La diferencia en edad es", edad != 18) // false
-// Diferencia por tipo
-console.log("La diferencia en edad es", edad !== "18") // true
-
-// mayor que y menor que
-
-console.log("Mayor que edad", 20 > edad); // true
-
-console.log("Menor que edad ", 15 < edad); // true
-
-console.log("Prueba de fuego menor que", 18 < edad) // false
-
-// mayor igual y menor igual
-
-console.log("Mayor igual ", 18 >= edad) // true
-console.log("Menor igual ", 18 <= edad) // true
-
-// Operadores and (Y)operadores  OR (O)
-
-// Operador Y &&
-
-var edad = 26;
-var genero = "M";
-
-console.log(" Operador and  que si sucede", edad >= 18 && genero == "M" ) // True
-console.log(" Operador and  que no sucede", edad >= 18 && genero == "F" ) // False
-console.log(" Operador and  que no sucede", edad <= 18 && genero == "M" ) // False
-console.log(" Operador and  que no sucede", edad <= 18 && genero == "F" ) // False
-
-// Operador O  ||
-
-console.log(" Operador or  que si sucede", edad >= 18 || genero == "M" ) // True
-console.log(" Operador or  que si sucede", edad >= 18 || genero == "F" ) // True
-console.log(" Operador or  que si sucede", edad <= 18 || genero == "M" ) // True
-console.log(" Operador or  que no sucede", edad <= 18 || genero == "F" ) // False
+// console.log(x - y); //NaN
 
 
-// Setencia de control IF
 
-// = ---> asignacion
-// == comparación por valor
-// === comparacion por tipo y valor
+// // Operadores Logicos
 
-var edad = prompt("¿Cuel es tu edad?");
+// var edad = 18;
 
-if(edad >= 18){
-    alert("Ya eres todo un señor 🤠");
-}
-else if(edad == 15){
-    alert("Para cuando la fiesta 🌚");
-}else if(edad < 18){
-    alert("Estas chiquito 👶🏻")
-}
-else{
-    //Cualquier otra cosa que no estoy contemplando
-    alert("No eres humano 👽");
-}
+// // Comparación por valor
+
+// console.log("La comparación por valor de edad es :", edad == 18); //True
+
+// // Comparacion por tipo y valor
+// console.log("La comparación por tipo de edad es :", edad === "18"); //False
+
+// // Diferente por valor
+
+// console.log("La diferencia en edad es", edad != 18) // false
+// // Diferencia por tipo
+// console.log("La diferencia en edad es", edad !== "18") // true
+
+// // mayor que y menor que
+
+// console.log("Mayor que edad", 20 > edad); // true
+
+// console.log("Menor que edad ", 15 < edad); // true
+
+// console.log("Prueba de fuego menor que", 18 < edad) // false
+
+// // mayor igual y menor igual
+
+// console.log("Mayor igual ", 18 >= edad) // true
+// console.log("Menor igual ", 18 <= edad) // true
+
+// // Operadores and (Y)operadores  OR (O)
+
+// // Operador Y &&
+
+// var edad = 26;
+// var genero = "M";
+
+// console.log(" Operador and  que si sucede", edad >= 18 && genero == "M" ) // True
+// console.log(" Operador and  que no sucede", edad >= 18 && genero == "F" ) // False
+// console.log(" Operador and  que no sucede", edad <= 18 && genero == "M" ) // False
+// console.log(" Operador and  que no sucede", edad <= 18 && genero == "F" ) // False
+
+// // Operador O  ||
+
+// console.log(" Operador or  que si sucede", edad >= 18 || genero == "M" ) // True
+// console.log(" Operador or  que si sucede", edad >= 18 || genero == "F" ) // True
+// console.log(" Operador or  que si sucede", edad <= 18 || genero == "M" ) // True
+// console.log(" Operador or  que no sucede", edad <= 18 || genero == "F" ) // False
+
+
+// // Setencia de control IF
+
+// // = ---> asignacion
+// // == comparación por valor
+// // === comparacion por tipo y valor
+
+// var edad = prompt("¿Cuel es tu edad?");
+
+// if(edad >= 18){
+//     alert("Ya eres todo un señor 🤠");
+// }else if(edad == 15){
+//     alert("Para cuando la fiesta 🌚");
+// }else if(edad < 18){
+//     alert("Estas chiquito 👶🏻")
+// }
+// else{
+//     //Cualquier otra cosa que no estoy contemplando
+//     alert("No eres humano 👽");
+// }
+
+
+
 
 
 
